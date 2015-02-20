240dadbaecfddc0f145fbfb6226a2a18f1495547
Lukas Matt <lukas@zauberstuhl.de>
2015-02-20 12:28:38 +0100

    Added public assets

---
+++ public/javascripts/autogen_stats.js
@@ -0,0 +1,13 @@
+var AWSTATS = [
+{"period": "2015-01-00 00:00:00", "all": "16925", "unique": "5856"}
+,{"period": "2015-02-00 00:00:00", "all": "11954", "unique": "4357"}
+,{"period": "2014-05-00 00:00:00", "all": "3378", "unique": "1673"}
+,{"period": "2014-06-00 00:00:00", "all": "16966", "unique": "4826"}
+,{"period": "2014-07-00 00:00:00", "all": "16714", "unique": "5187"}
+,{"period": "2014-08-00 00:00:00", "all": "14958", "unique": "4716"}
+,{"period": "2014-09-00 00:00:00", "all": "17293", "unique": "5224"}
+,{"period": "2014-10-00 00:00:00", "all": "20238", "unique": "4994"}
+,{"period": "2014-11-00 00:00:00", "all": "19020", "unique": "5198"}
+,{"period": "2014-12-00 00:00:00", "all": "17681", "unique": "5126"}
+];
+var DSTATS = {"name":"Sechat*","network":"Diaspora","version":"head-pdaaeec7d","registrations_open":false,"services":["twitter","tumblr","facebook","wordpress"],"total_users":1397,"active_users_monthly":59,"active_users_halfyear":311,"local_posts":2740,"local_comments":1824,"twitter":true,"tumblr":true,"facebook":true,"wordpress":true};
