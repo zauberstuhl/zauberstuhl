872c3e278712d290d15e5be9678565f56c025eb7
Lukas Matt <lukas@zauberstuhl.de>
2015-02-20 13:59:15 +0100

    Port html to scala templates and add dependencies

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,11 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Zauberstuhl Network"))
+  }
+
+  def statistics = Action {
+    Ok(views.html.statistics("Sechat* Statistics"))
   }
 
 }
