fcfa5126dca1a506b6fa3c49838a8ac9192f2cb4
Lukas Matt <lukas@zauberstuhl.de>
2015-02-22 00:05:23 +0100

    Add custom 400, 404 and 500 error page

---
+++ app/helpers/Global.scala
@@ -0,0 +1,26 @@
+package helpers
+
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent.Future
+
+object Global extends GlobalSettings {
+  override def onError(req: RequestHeader, ex: Throwable) = {
+    Future.successful(InternalServerError(views.html.error(
+      "500 Internal Server Error",
+      "Outsch .. What have you done :?")))
+  }
+
+  override def onBadRequest(req: RequestHeader, error: String) = {
+    Future.successful(NotFound(views.html.error(
+      "400 Bad Request",
+      "The request could not be understood by the server!")))
+  }
+
+  override def onHandlerNotFound(req: RequestHeader) = {
+    Future.successful(NotFound(views.html.error(
+      "404 Not Found",
+      "The requested URL was not found on the server!")))
+  }
+}
